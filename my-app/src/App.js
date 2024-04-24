2e24156f00a71cb33bc116884df7e6970c186fcd
Alexandr Blinov <alexandr_b_2000@mail.ru>
2024-04-24 20:19:43 +0300

    changes in App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,39 @@
+import { createElement } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-// Это всё декларативный стиль
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<div>{new Date().getFullYear()}</div>
-			</header>
-		</div>
+	const year = new Date().getFullYear();
+	return createElement(
+		"div",
+		{ className: "App" },
+		createElement(
+			"header",
+			{ className: "App-header" },
+			createElement("img", {
+				src: logo,
+				className: "App-logo",
+				alt: logo,
+			}),
+			createElement(
+				"p",
+				null,
+				"Edit ",
+				createElement("code", null, "src/App.js "),
+				"and save to reload."
+			),
+			createElement(
+				"a",
+				{
+					className: "App-link",
+					href: "https://reactjs.org",
+					target: "_blank",
+					rel: "noopener noreferrer",
+				},
+				"Learn React"
+			),
+			createElement("div", null, year)
+		)
 	);
 }
-
 export default App;
